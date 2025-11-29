c70c9e4d26cce0983d89ff327cdfd0571048e2c5
장우준 <lambda@smail.kongju.ac.kr>
2025-11-29 20:59:06 +0900

    init5

--- app/src/main/java/com/javaprogemming/uwbproject/UwbManager.kt
+++ app/src/main/java/com/javaprogemming/uwbproject/UwbManager.kt
@@ -13,6 +13,7 @@ import androidx.core.uwb.UwbManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
